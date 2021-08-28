2f1cbedead1891b24bfc38ffb4154cb6574ac1f9
Isa SUMER <sumerisa5@gmail.com>
2021-08-28 10:03:36 +0300

    button click added

---
+++ app.js
@@ -0,0 +1,19 @@
+
+
+let liste = document.querySelector("#list")
+let task = document.querySelector("#task"); 
+let liveToastBtn = document.querySelector("#liveToastBtn")
+
+liveToastBtn.addEventListener("click",function(){
+    // createElement ile yeni bir listItem (li) oluşturuyoruz
+    var li=document.createElement("li"); 
+    
+    // Input ile gelen veriyi "li" içerisine metin olarak giriyoruz.
+    li.textContent=task.value; 
+    
+    // appendChild ile oluşturduğumuz elementi "liste" isimli değişkenimizin içerisine ekliyoruz.
+    liste.appendChild(li);
+                
+    // Veri inputu içerisindeki metni siliyoruz.
+    task.value = "";
+  });
